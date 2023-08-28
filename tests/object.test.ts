5b1144014cba8313e214168d0680096268aa10be
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2023-08-29 00:39:01 +0700

    object type

---
+++ tests/object.test.ts
@@ -0,0 +1,15 @@
+describe("Object", function () {
+  it("should support in typesript", function () {
+    const person: { id: string; name: string } = {
+      id: "1",
+      name: "Kevin",
+    };
+
+    console.info(person);
+
+    person.id = "2";
+    person.name = "Sinaga";
+
+    console.info(person);
+  });
+});
